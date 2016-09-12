7a873c3f0ae0bb90655da95d50e512f4af176a73
Rich Pelavin <rich.reactor8.com>
2016-09-12 01:09:45 +0000

    TK-2651: Put in logic to process workflows when 'dtk service push' invoked

--- lib/dsl/template/v1/workflow.rb
+++ lib/dsl/template/v1/workflow.rb
@@ -18,43 +18,27 @@
 class DTK::DSL::Template
   class V1
     class Workflow < self
-      # TODO: DTK-2554: workflows: Aldin: 
-      # cut and paste from import_task_templates and its subfunctions in 
-      # https://github.com/dtk/dtk-server/blob/DTK-2554/application/model/module/service_module/dsl/assembly_import/adapters/v4.rb
-      # might break this into multiple files underneath this to handle sub parts of workflow
-      # these dont have to be templates; just files included by # require_relative('workflow/...)
+      require_relative('workflow/semantic_parse')
+
       module Constant
         module Variations
         end
         
         extend ClassMixin::Constant
-        
-        CreateWorkflowAction = 'create'
       end
       
       def parser_output_type
         :hash
       end
       
-      def self.parse_elements(input, parent_info)
-        workflows =
-          if input_hash?(input)
-            [input]
-          elsif input_array?(input)
-            input
-          else
-            raise parsing_error("Ill-formed workflow(s) section")
-          end
-        
-        ret = file_parser_output_hash
-        workflows.each_with_index do |workflow, i|
-          ret.merge!(parse_element(workflow, parent_info, :index => name?(workflow) || i))
+      def self.parse_elements(input_hash, parent_info)
+        input_hash.inject(file_parser_output_hash) do |h, (name, workflow)|
+          h.merge(name => parse_element(workflow, parent_info, :index => name))
         end
-        ret
       end
       
       def parse!
-        input_hash.each_pair { |name, workflow_hash| merge(parse_workflow(name, workflow_hash)) }
+        merge parse_workflow(input_hash)
       end
 
       ### For generation
@@ -70,34 +54,11 @@ class DTK::DSL::Template
       
       private
       
-      def self.name?(workflow)
-        if input_hash?(workflow)
-          workflow.keys.first if workflow.size == 1
-        end
+      def parse_workflow(workflow_hash)
+        # TODO: put in fine grain parsing of workflow_hash
+        workflow_hash
       end
-      
-      def parse_workflow(name, workflow_hash)
-        # Put in fine grain parsing
-        # TODO: change to canonical output form and make output be array that has canonical fields for workflow_hash
-        # TODO: handle normalize create to be on server side
-        normalized_name = normalized_name(name)
-        { normalized_name => {
-            'task_action' => normalized_name,
-            'content'     => workflow_hash
-          }
-        }
-      end
-      
-      INTERNAL_CREATE_ACTION_NAME = '__create_action'
 
-      def normalized_name(name)
-        if name.nil? or Constant.matches?(name, :CreateWorkflowAction)
-          '__create_action'
-        else
-          name
-        end
-
-      end
     end
   end
 end
